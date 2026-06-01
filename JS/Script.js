7ead3b466152488c10a1df7a3f32bd66415d8324
Miranda <263408682+rrmiranda243@users.noreply.github.com>
2026-06-01 21:36:44 +0100

    Banner
    Criação do Banner no index.html
    imagem de exemplo no banner e codigo no js sobre o banner

--- JS/Script.js
+++ JS/Script.js
@@ -9,3 +9,16 @@ const showMenu = (toggleId, navId) =>{
 }
 
 showMenu('nav-toggle','navMenu')
+
+
+//Slides
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+   pagination: {
+        el: ".swiper-pagination",
+      },
+});
